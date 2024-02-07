2a7823d23fa23a65133e7ef44fad79985014d945
Jason Luck <jluck@meetveracity.com>
2024-02-07 09:38:55 -0600

    adding harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,14 +6,14 @@ return {
     local harpoon = require("harpoon")
     harpoon.setup()
 
+    vim.keymap.set("n", "<C-e>", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
     vim.keymap.set("n", "<leader>a", function()
       harpoon:list():append()
     end)
     vim.keymap.set("n", "<leader>d", function()
       harpoon:list():remove()
-    end)
-    vim.keymap.set("n", "<C-e>", function()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
     end)
 
     vim.keymap.set("n", "<C-h>", function()
