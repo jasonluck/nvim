90dafaea95411a51b8bba7147fb0f1ef706a36b9
Jason Luck <jluck@meetveracity.com>
2024-02-09 13:16:36 -0600

    adding autotab plugin to tab out of autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,5 +1,15 @@
 return {
-  "windwp/nvim-autopairs",
-  event = "InsertEnter",
-  opts = {}, -- this is equalent to setup({}) function
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {}, -- this is equalent to setup({}) function
+	},
+
+	--Tabout plugin to allow us to tab out of our autopaired brackets, quotes, ..
+	{
+		"abecodes/tabout.nvim",
+		config = function()
+			require("tabout").setup()
+		end,
+	},
 }
